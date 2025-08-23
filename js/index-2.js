1afd658c326c8ea71b116cb7068a1593fe2a241c
istiaquerijve <istiaquerijve@gmail.com>
2025-08-23 16:40:38 +0600

    commit

--- js/index-2.js
+++ js/index-2.js
@@ -9,8 +9,39 @@ document.getElementById("add-money-btn").addEventListener("click", function (e) 
     const pin = document.getElementById("add-pin").value
 
     const avBalance = parseInt(document.getElementById("balance").innerText)
-    console.log(avBalance);
+    // console.log(avBalance);
     
+    if(accNumber.length < 11 && accNumber.length > 11){
+        alert('provide full number')
+        return;
+    }
+
+
     const totalMoney = amount + avBalance;
     document.getElementById("balance").innerText = totalMoney
 })
+
+
+//cash out
+
+document.getElementById("out-btn").addEventListener("click", function (e) {
+    e.preventDefault()
+    const amount = parseInt(document.getElementById("out-btn").value)
+    const avBalance = parseInt(document.getElementById("balance").innerText)
+    const totalBalance = avBalance - amount
+    document.getElementById("balance").innerText = totalBalance
+})
+
+
+// toggling
+
+document.getElementById("money-in").addEventListener("click", function () {
+    document.getElementById("cash-out").style.display = "none"
+    document.getElementById("add-money").style.display = "block"
+})    
+
+
+document.getElementById("money-out").addEventListener("click", function () {
+    document.getElementById("add-money").style.display = "none"
+    document.getElementById("cash-out").style.display = "block"
+})    
